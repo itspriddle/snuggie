49c297c5cb2f5f86e664ad9ff7e81b9824c87155
Joshua Priddle <jpriddle@nevercraft.net>
2011-06-15 09:29:20 -0400

    Remove Config#initialize, we don't need a default username/password set

--- lib/snuggie/config.rb
+++ lib/snuggie/config.rb
@@ -2,10 +2,5 @@ module Snuggie
   class Config
     attr_accessor :username
     attr_accessor :password
-
-    def initialize
-      @username = 'username'
-      @password = 'password'
-    end
   end
 end
--- test/snuggie/config_test.rb
+++ test/snuggie/config_test.rb
@@ -5,16 +5,6 @@ context "Snuggie::Config" do
     @config = Snuggie::Config.new
   end
 
-  test "#initialize sets @username" do
-    assert_instance_var @config, :username
-    assert_equal @config.instance_variable_get(:@username), 'username'
-  end
-
-  test "#initialize sets @password" do
-    assert_instance_var @config, :password
-    assert_equal @config.instance_variable_get(:@password), 'password'
-  end
-
   test "has attr_accessor :username" do
     assert_attr_accessor @config, :username
   end
